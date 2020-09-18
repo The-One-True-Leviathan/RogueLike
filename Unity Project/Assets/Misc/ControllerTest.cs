17503a3b23007cdb594f503e06bd81f7af6ce0af
Leviathan <tryskhell@gmail.com>
2020-09-18 18:24:13 +0200

    Modified ControllerTest.cs

--- Unity Project/Assets/Misc/ControllerTest.cs
+++ Unity Project/Assets/Misc/ControllerTest.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class ControllerTest : MonoBehaviour
 {
     Controler controller;
+    Rigidbody rigidbody;
+    public float maxSpeed;
     public float xAxis;
     public float yAxis;
     // Start is called before the first frame update
 
     private void OnEnable()
     {
+        rigidbody = GetComponent<Rigidbody>();
         controller = new Controler();
         controller.Enable();
     }
@@ -22,8 +25,11 @@ public class ControllerTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        xAxis = controller.Keyboard.Movement.ReadValue<Vector2>().x;
-        yAxis = controller.Keyboard.Movement.ReadValue<Vector2>().y;
+        xAxis = controller.Keyboard.Movement.ReadValue<Vector2>().x*maxSpeed;
+        yAxis = controller.Keyboard.Movement.ReadValue<Vector2>().y * maxSpeed;
         print("xAxis = " + xAxis + " / yAxis = " + yAxis);
+        Vector3 speed = new Vector3(xAxis, 0f, yAxis);
+        rigidbody.velocity = speed;
+        
     }
 }
