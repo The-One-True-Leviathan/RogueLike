c7db3ff863cf1d6d08d6ada67432c82fd4917e86
LAPTOP-CQNK8T9C\ingri <j.bourdin@rubika-edu.com>
2020-09-19 18:15:06 +0200

    HealthBarATester

---
+++ Unity Project/Assets/Scripts/Julia/HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image bar;
+    public int fill;
+    public int vieTemp;
+    public UnityEngine.UI.Text displayLife;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fill = 20;
+        vieTemp = fill;
+        bar.fillAmount = fill;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        displayLife.text = vieTemp.ToString() + "/" + fill.ToString();
+    }
+    public void ApplyDamage(int damage)
+    {
+        vieTemp -= damage;
+        bar.fillAmount = vieTemp;
+    }
+        
+    
+}
