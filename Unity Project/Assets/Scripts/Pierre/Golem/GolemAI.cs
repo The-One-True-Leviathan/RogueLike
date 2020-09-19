33af89d70ef1c12035db10dc37bdc9cfcfc5a841
Leviathan <tryskhell@gmail.com>
2020-09-19 16:28:51 +0200

    Started working on Golem AI

---
+++ Unity Project/Assets/Scripts/Pierre/Golem/GolemAI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemAI : MonoBehaviour
+{
+
+    GameObject player;
+    
+    //Line of Sight
+    public float maxSight; //How far can the enemy see ?
+    public LayerMask blockLOS;
+    public LayerMask isPlayer;
+    Vector3 golemToPlayer;
+    public float distanceToPlayer, angleToPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player)
+        {
+            golemToPlayer = player.transform.position - transform.position;
+            distanceToPlayer = golemToPlayer.magnitude;
+        
+            Sightcast();
+        }
+    }
+
+    void Sightcast() 
+    {
+        bool hit = Physics.Raycast(transform.position, golemToPlayer, maxSight, isPlayer);
+        if (hit)
+        {
+            print("Player in sight !");
+        }
+
+
+        Debug.DrawRay(transform.position, golemToPlayer * maxSight, Color.red);
+    }
+}
