d79dce6fd0f7ce4605a32c196b6d1c7782907537
LAPTOP-CQNK8T9C\ingri <j.bourdin@rubika-edu.com>
2020-09-21 12:32:23 +0200

    Ajout menu principal

---
+++ Unity Project/Assets/Scripts/Julia/MenuPrincipal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("HUD");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
