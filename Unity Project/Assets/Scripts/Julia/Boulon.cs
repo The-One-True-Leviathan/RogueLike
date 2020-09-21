77458136ef13caae93e03c997a78b2c6fc7ef221
LAPTOP-CQNK8T9C\ingri <j.bourdin@rubika-edu.com>
2020-09-21 10:58:08 +0200

    Rajout des scripts pour boulons et pièces (à tester) avec leur morceau du HUD

---
+++ Unity Project/Assets/Scripts/Julia/Boulon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boulon : MonoBehaviour
+{
+    public int nbreBoulon;
+    public UnityEngine.UI.Text displayBoulon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //mettre l'animation iddle
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        displayBoulon.text = "Boulons:" + nbreBoulon.ToString();
+    }
+
+    void OnCollisionEnter(Collision collisionBoulon) //il faut que l'un des colliders soit avec un non-kinematic rigidbody
+    {
+        //son récupération
+        nbreBoulon++;
+        Destroy(gameObject, 1);
+    }
+
+}
---
+++ Unity Project/Assets/Scripts/Julia/Piecette.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Piecette : MonoBehaviour
+{
+    public int nbrePiecettes;
+    public UnityEngine.UI.Text displayPiecettes;
+    // Start is called before the first frame update
+    void Start()
+    { 
+        //animation iddle
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        displayPiecettes.text = "Piecettes:" + nbrePiecettes.ToString();
+    }
+
+    private void OnCollisionEnter(Collision collisionPiecettes)
+    {
+        //son récupération
+        nbrePiecettes ++;
+        Destroy(gameObject, 1);
+    }
+}
