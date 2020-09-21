5d397d653545b1b565839094852d8717bd5b5209
LAPTOP-CQNK8T9C\ingri <j.bourdin@rubika-edu.com>
2020-09-21 15:49:30 +0200

    tests UI, peu concluants

--- Unity Project/Assets/Scripts/Julia/MenuPrincipal.cs
+++ Unity Project/Assets/Scripts/Julia/MenuPrincipal.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Composites;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuPrincipal : MonoBehaviour
 {
+
+    private void OnMouseEnter()
+    {
+        
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene("HUD");
