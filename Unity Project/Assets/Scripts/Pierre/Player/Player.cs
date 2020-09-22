a1b20e8aa350f3d636e67947e3a3233aa3c21e96
Leviathan <tryskhell@gmail.com>
2020-09-22 11:57:54 +0200

    Added Player Attack Prototype, and player Script, as well as Weapon Scriptable Objects

---
+++ Unity Project/Assets/Scripts/Pierre/Player/Player.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Weapons;
+
+public class Player : MonoBehaviour
+{
+    public LayerMask layerEnemies;
+
+
+    Controler controller;
+
+
+
+
+
+
+
+
+
+
+
+
+    public bool dualWielding; //Is the character wielding two different weapons ?
+    public WeaponScriptableObject weapon1, weapon2, weaponInHitSpan, switchSpace; //Weapon 1 and 2 are the two "hands" of the player, weaponInHitSpan is used for multi-frame attacks, and switchSpace is only used 
+                                                                                  //when switching weapons in both hands
+    public bool isInBuildup, isInCharge, isInAttack, isInRecover, isInCooldown, isInHitSpan;
+    public Vector3 attackDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = new Controler();
+        controller.Enable();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*  if (!isInAttack && !isInCooldown) 
+         *  {
+         *      if(bouton attaquer 1)
+         *      {
+         *          Attack(weapon1);
+         *      }
+         *  
+         *      if(bouton attaquer 2)
+         *      {
+         *          if (dualWielding)
+         *          {
+         *              Attack(weapon2);
+         *          } 
+         *          else 
+         *          {
+         *              Attack(weapon1);
+         *          }
+         *      }
+         *  }
+         *  
+         *  if (isInHitSpan) 
+         *  {
+         *      HitSpan(weaponInHitSpan);
+         *  }
+         */
+        attackDirection = new Vector3(controller.Keyboard.Movement.ReadValue<Vector2>().x, 0, controller.Keyboard.Movement.ReadValue<Vector2>().y);
+        Debug.DrawRay(transform.position, attackDirection, Color.red);
+        if (isInHitSpan)
+        {
+            HitSpan(weaponInHitSpan);
+        }
+    }
+
+    public void Attack(WeaponScriptableObject weapon)
+    {
+
+    }
+
+    IEnumerator ResolveAttack(WeaponScriptableObject weapon, bool main)
+    {
+        print("start attack");
+        yield return new WaitForSeconds(weapon.atk1Buildup);
+        print("attack");
+        isInHitSpan = true;
+        weaponInHitSpan = weapon;
+
+        yield return new WaitForSeconds(weapon.atk1Recover);
+        print("recover");
+        yield return new WaitForSeconds(weapon.atk1Cooldown - weapon.atk1Recover);
+        print("cooldown");
+    }
+
+    public void HitSpan(WeaponScriptableObject weapon)
+    {
+        if(!weapon.atk1Ranged)
+        {
+            Collider[] hitEnemies = Physics.OverlapSphere(transform.position, weapon.atk1Reach.z, layerEnemies);
+            foreach (Collider enemy in hitEnemies)
+            {
+                Vector3 enemyDirection = enemy.transform.position - transform.position;
+                Debug.DrawRay(transform.position, enemyDirection, Color.red);
+                float enemyAngle = Vector3.Angle(attackDirection, enemyDirection);
+                print(enemyAngle);
+                if (enemyAngle <= weapon.atk1Reach.x)
+                {
+                    print("Enemy hit !");
+                }
+
+            }
+        }
+    }
+}
---
+++ Unity Project/Assets/Scripts/Pierre/Weapons/ScriptableObject/WeaponScriptableObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Weapons
+{
+
+    [CreateAssetMenu(fileName = "newWeapon", menuName = "Pierre/Weapon", order = 0)]
+    public class WeaponScriptableObject : ScriptableObject
+    {
+        public Sprite weaponSprite;
+        public string weaponName;
+        public int weaponID;
+        GameObject player;
+
+        //Attack 1
+        public bool atk1Heavy; //Is the attack heavy ? IE, can the player turn, roll or move during the buildup ?
+
+        public bool atk1Ranged; //Is the attack a projectile ?
+        public GameObject atk1Projectile; //What does the attack fire exactly ?
+
+        public Vector3 atk1Reach; //Informs the size of the attack in 3 dimensions, generally set to 0 in ranged attacks (but not necessarily). X is equal to the angle of "leeway", Z is equal to the length of the attack
+
+        public bool atk1Charge; //Can the attack be charged ?
+        public float atk1Charge0, atk1Charge1, atk1Charge2; //Charge 0 is the time after which the attack can be used, charge 1 and 2 are two steps of more powerful attacks
+
+        public float atk1Buildup, atk1HitSpan, atk1Recover, atk1Cooldown; //Hitspan is the time during which the hitbox persists, or the lifetime of the projectile, if any
+                                                                          //Recover is the time after the buildup during which the character cannot roll or interact with the environment
+                                                                          //Cooldown is the time after the buildup during which the character cannot attack again
+        public int atk1Damage; //The number of HP reduced from the enemy health
+        public float atk1KnockBack; //How forcefully is the enemy thrown back
+
+    }
+
+}
+
