e7fbdaceebd46cdba9dfa906b8f1b1af550cfb32
ETU\r.pelletierpalumbo <r.pelletierpalumbo@rubika-edu.com>
2020-09-28 17:20:01 +0200

    début du code super avancé de GENIE qui permet de fliper des tables

--- Unity Project/Assets/RPP_Docs/RPP_Scripts/TableFlip.cs
+++ Unity Project/Assets/RPP_Docs/RPP_Scripts/TableFlip.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.Remoting.Messaging;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TableFlip : MonoBehaviour
 {
+    public List<GameObject> indicatorObject = new List<GameObject>();
     public List<Transform> indicatorPosition = new List<Transform>();
     public List<Vector3> overlapSize = new List<Vector3>();
     public LayerMask currentLayer;
+    public Controler controler;
     bool tableIsFliped, canFlipBack, canFlipTop, canFlipLeft, canFlipRight;
 
     private void Start()
     {
         tableIsFliped = false;
+        foreach (GameObject obj in indicatorObject)
+        {
+            obj.SetActive(false);
+        }
     }
 
     private void FixedUpdate()
@@ -21,11 +30,31 @@ public class TableFlip : MonoBehaviour
         {
             DrawOverlaps();
         }
+        controler.Keyboard.Interact.performed += ctx => FlipTheFuckingTable();
     }
 
     void FlipTheFuckingTable()
     {
-       //if ()
+       if (canFlipBack)
+       {
+            //flip the table backwards
+            tableIsFliped = true;
+       }
+       else if (canFlipTop)
+       {
+            //flip the table on top
+            tableIsFliped = true;
+       }
+       else if (canFlipLeft)
+       {
+            //flip the table left
+            tableIsFliped = true;
+       }
+       else if (canFlipRight)
+       {
+            //flip th table right
+            tableIsFliped = true;
+       }
     }
 
     void DrawOverlaps()
@@ -35,15 +64,21 @@ public class TableFlip : MonoBehaviour
         Collider[] leftDetecor = Physics.OverlapBox(indicatorPosition[2].position, overlapSize[2] / 2, Quaternion.identity, currentLayer);
         Collider[] rightDetecor = Physics.OverlapBox(indicatorPosition[3].position, overlapSize[3] / 2, Quaternion.identity, currentLayer);
 
+        indicatorObject[0].SetActive(false);
+        canFlipBack = false;
+        indicatorObject[1].SetActive(false);
+        canFlipTop = false;
+        indicatorObject[2].SetActive(false);
+        canFlipLeft = false;
+        indicatorObject[3].SetActive(false);
+        canFlipRight = false;
+
         foreach (Collider obj in backDetecor)
         {
             if (obj.CompareTag("Player"))
             {
+                indicatorObject[0].SetActive(true);
                 canFlipBack = true;
-            }
-            else
-            {
-                canFlipBack = false;
             }
         }
 
@@ -51,11 +86,8 @@ public class TableFlip : MonoBehaviour
         {
             if (obj.CompareTag("Player"))
             {
+                indicatorObject[1].SetActive(true);
                 canFlipTop = true;
-            }
-            else
-            {
-                canFlipTop = false;
             }
         }
 
@@ -63,11 +95,8 @@ public class TableFlip : MonoBehaviour
         {
             if (obj.CompareTag("Player"))
             {
+                indicatorObject[2].SetActive(true);
                 canFlipLeft = true;
-            }
-            else
-            {
-                canFlipLeft = false;
             }
         }
 
@@ -75,12 +104,18 @@ public class TableFlip : MonoBehaviour
         {
             if (obj.CompareTag("Player"))
             {
+                indicatorObject[3].SetActive(true);
                 canFlipRight = true;
             }
-            else
-            {
-                canFlipRight = false;
-            }
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(indicatorPosition[0].position, overlapSize[0]);
+        Gizmos.DrawWireCube(indicatorPosition[1].position, overlapSize[1]);
+        Gizmos.DrawWireCube(indicatorPosition[2].position, overlapSize[2]);
+        Gizmos.DrawWireCube(indicatorPosition[3].position, overlapSize[3]);
     }
 }
