023393dd2946db84ee1e640c9ff260106f996636
LAPTOP-CQNK8T9C\ingri <j.bourdin@rubika-edu.com>
2020-09-30 13:05:45 +0200

    Add item class, item behavior and item description scripts, not tested yet

---
+++ Unity Project/Assets/Scripts/Julia/ItemBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace items
+{
+
+    public class ItemBehavior : MonoBehaviour
+    {
+        public ItemScriptableObject itemScriptableObject;
+        public HealthBar healthBar;
+        // Start is called before the first frame update
+        void Start()
+        {
+            healthBar = GameObject.FindGameObjectWithTag("HUD").GetComponent<HealthBar>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnCollisionEnter(Collision player)
+        {
+            if (player.gameObject.tag == "Player" && itemScriptableObject.isFromShop == false)
+            {
+                ApplyEffect();
+            }
+        }
+
+        public void ApplyEffect()
+        {
+            if (itemScriptableObject.itemType == ItemType.Heal)
+            {
+                healthBar.ApplyDamage(-itemScriptableObject.strength);
+            }
+            else if (itemScriptableObject.itemType == ItemType.MaxPlus)
+            {
+                healthBar.UpgradeLife(itemScriptableObject.strength);
+            }
+            Destroy(gameObject, 1);
+        }
+    }
+}
---
+++ Unity Project/Assets/Scripts/Julia/ObjectItemCard.cs
@@ -0,0 +1,35 @@
+﻿using items;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectItemCard : MonoBehaviour
+{
+    public GameObject objectUI;
+    public ItemScriptableObject item;
+    public Text[] texts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        texts = GetComponentsInChildren<Text>();
+        texts[0].text = item.itemName;
+        texts[1].text = item.itemDescription;
+    }
+
+    private void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            objectUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            objectUI.SetActive(false);
+        }
+    }
+}
