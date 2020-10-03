93b970691581c1863fad907b2247b3846a47fa6a
Portable Pierre <tryskhell@gmail.com>
2020-10-03 15:18:13 +0200

    Tested and debugged Inventory

--- Unity Project/Assets/Scripts/Julia/Inventory.cs
+++ Unity Project/Assets/Scripts/Julia/Inventory.cs
@@ -12,6 +12,7 @@ public class Inventory : MonoBehaviour
     public Image[] weaponImages;
     public Text[] texts;
     public bool isOpen = false;
+    public List<string> displayEnchantInventory1, displayDescriptionEnchant1, displayEnchantInventory2, displayDescriptionEnchant2;
 
     // Start is called before the first frame update
     void Start()
@@ -22,12 +23,19 @@ public class Inventory : MonoBehaviour
         texts = GetComponentsInChildren<Text>();
         weaponImages = GetComponentsInChildren<Image>();
         inventoryItem = gameObject;
+        for (int i = 0; i < 4; i++)
+        {
+            displayEnchantInventory1.Add("<enchantment slot>");
+            displayDescriptionEnchant1.Add("<enchant this by attacking a book>");
+            displayEnchantInventory2.Add("<enchantment slot>");
+            displayDescriptionEnchant2.Add("<enchant this by attacking a book>");
+        }
     }
 
     void UpdateInventory()
     {
-        string[] displayEnchantInventory1 = new string[4];
-        string[] displayDescriptionEnchant1 = new string[4];
+        player.weapon1.InitializeWeapon();
+        player.weapon2.InitializeWeapon();
         for (int i = 0; i < 4; i++)
         {
             displayEnchantInventory1[i] = "<enchantment slot>";
@@ -35,26 +43,25 @@ public class Inventory : MonoBehaviour
         }
         for (int i = 0; i < player.weapon1.enchantments.Count; i++)
         {
-            if (player.weapon1.enchantments[i])
-            {
-                displayEnchantInventory1[i] = player.weapon1.enchantments[i].name;
-                displayDescriptionEnchant1[i] = player.weapon1.enchantments[i].description;
-            }
+            displayEnchantInventory1[i] = player.weapon1.enchantments[i].name;
+            texts[i + 2].color = Color.grey;
+            texts[i + 2].color = player.weapon1.enchantments[i].color;
+            displayDescriptionEnchant1[i] = player.weapon1.enchantments[i].description;
+            texts[i + 12].color = Color.grey;
+            texts[i + 12].color = player.weapon1.enchantments[i].color;
+
         }
         for (int i = 0; i < 4; i++)
         {
-                texts[i + 2].text = displayEnchantInventory1[i];
-                texts[i + 2].color = player.weapon1.enchantments[i].color;
-                texts[i + 12].text = displayDescriptionEnchant1[i];
+            texts[i + 2].text = displayEnchantInventory1[i];
+            texts[i + 12].text = displayDescriptionEnchant1[i];
         }
         texts[0].text = player.weapon1.weaponRealName;
         texts[0].color = player.weapon1.weaponColor;
         texts[1].text = player.weapon1.weaponDescription;
-        weaponImages[1].sprite = player.weapon1.weaponItemSprite;
+        weaponImages[1].sprite = player.weapon1.weaponIcon;
         if (player.dualWielding)
         {
-            string[] displayEnchantInventory2 = new string[4];
-            string[] displayDescriptionEnchant2 = new string[4];
             for (int i = 0; i < 4; i++)
             {
                 displayEnchantInventory2[i] = "<enchantment slot>";
@@ -62,51 +69,52 @@ public class Inventory : MonoBehaviour
             }
             for (int i = 0; i < player.weapon2.enchantments.Count; i++)
             {
-                if (player.weapon2.enchantments[1])
-                {
-                    displayEnchantInventory2[i] = player.weapon2.enchantments[i].name;
-                    displayDescriptionEnchant2[i] = player.weapon2.enchantments[i].description;
-                }
+                displayEnchantInventory2[i] = player.weapon2.enchantments[i].name;
+                texts[i + 8].color = Color.grey;
+                texts[i + 8].color = player.weapon2.enchantments[i].color;
+                displayDescriptionEnchant2[i] = player.weapon2.enchantments[i].description;
+                texts[i + 16].color = Color.grey;
+                texts[i + 16].color = player.weapon2.enchantments[i].color;
+
             }
             for (int i = 0; i < 4; i++)
             {
-                    texts[i + 8].text = displayEnchantInventory2[i];
-                    texts[i + 8].color = player.weapon2.enchantments[i].color;
-                    texts[i + 16].text = displayDescriptionEnchant2[i];
+                texts[i + 8].text = displayEnchantInventory2[i];
+                texts[i + 16].text = displayDescriptionEnchant2[i];
             }
             texts[6].text = player.weapon2.weaponRealName;
             texts[6].color = player.weapon2.weaponColor;
             texts[7].text = player.weapon2.weaponDescription;
-            weaponImages[2].sprite = player.weapon2.weaponItemSprite;
+            weaponImages[2].sprite = player.weapon2.weaponIcon;
         }
         else if (player.dualWielding == false)
         {
             texts[6].text = "<empty>";
             texts[7].text = "<no weapon>";
-            string[] displayEnchantInventory2 = new string[4];
-            string[] displayDescriptionEnchant2 = new string[4];
             for (int i = 0; i < 4; i++)
             {
-                displayEnchantInventory2[i] = "<enchantment slot>";
-                displayDescriptionEnchant2[i] = "<no weapon>";
+                displayEnchantInventory2[i] = "";
+                displayDescriptionEnchant2[i] = "";
                 texts[i + 8].text = displayEnchantInventory2[i];
                 texts[i + 16].text = displayDescriptionEnchant2[i];
             }
+            weaponImages[2].sprite = player.weapon1.weaponIconSecondary;
         }
     }
 
     private void Update()
     {
-        if (controller.Keyboard.Inventory.triggered && isOpen == false)
+        if (controller.Keyboard.Inventory.triggered)
         {
-            inventoryItem.SetActive(true);
+            isOpen = !isOpen;
             UpdateInventory();
-            isOpen = true;
         }
-        if(controller.Keyboard.Inventory.triggered && isOpen == true)
+        if(isOpen)
         {
-            inventoryItem.SetActive(false);
-            isOpen = false;
+            inventoryItem.GetComponent<RectTransform>().localScale = Vector3.one;
+        } else
+        {
+            inventoryItem.GetComponent<RectTransform>().localScale = Vector3.zero;
         }
     }
 }
