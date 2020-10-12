af898569fa786bfe6b15861c819bb00b3497ff0e
Leviathan <tryskhell@gmail.com>
2020-10-12 16:59:27 +0200

    Started Working on Procedural Generation

---
+++ Unity Project/Assets/Scripts/Pierre/Procedural Generation/GenManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenManager : MonoBehaviour
+{
+    public int roomAmount, maxRooms;
+    public List<GameObject> rooms;
+    public List<bool[][]> availableSpawners;
+    public List<Vector3> roomCoordinates = new List<Vector3>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity Project/Assets/Scripts/Pierre/Procedural Generation/RoomBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomBehavior : MonoBehaviour
+{
+    public int roomID;
+    public Vector2 roomCoo;
+    public GenManager genManager;
+    public List<GameObject> spawners;
+    bool isStartingRoom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        genManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GenManager>();
+        if (isStartingRoom)
+        {
+            genManager.roomCoordinates.Add(Vector3.zero);
+        }
+        genManager.rooms.Add(gameObject);
+        roomID = genManager.roomAmount;
+        genManager.roomAmount += 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
