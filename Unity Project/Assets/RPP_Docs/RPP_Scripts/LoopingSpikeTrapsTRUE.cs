86a03d8254d7e5e5ce4cf30ff91c0d575a5e6058
raphapelpal <60692478+raphapelpal@users.noreply.github.com>
2020-10-26 18:40:56 +0000

    Intégration Grafique des pièges à Piques

---
+++ Unity Project/Assets/RPP_Docs/RPP_Scripts/LoopingSpikeTrapsTRUE.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopingSpikeTrapsTRUE : MonoBehaviour
+{
+    public BoxCollider spikes;
+    public Animator spikesLoop;
+    public int spikesDamage;
+    bool canStartLoop;
+    private Transform spikeLocation;
+
+    //Player
+    public GameObject player;
+    public Player playerScript;
+
+    private void Start()
+    {
+        spikes.enabled = false;
+        canStartLoop = true;
+        spikesLoop.SetInteger("LoopSpikesInt", 1);
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<Player>();
+        spikeLocation = GetComponent<Transform>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (canStartLoop == true)
+        {
+            StartCoroutine(SpikesLoop());
+        }
+    }
+
+    IEnumerator SpikesLoop()
+    {
+        canStartLoop = false;        
+        spikes.enabled = true;
+        spikesLoop.SetInteger("LoopSpikesInt", 3);
+        OnTriggerEnter(spikes);
+        yield return new WaitForSeconds(0.3f);
+        spikes.enabled = false;
+        spikesLoop.SetInteger("LoopSpikesInt", 2);
+        yield return new WaitForSeconds(2f);
+        canStartLoop = true;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerScript.PlayerDamage(spikesDamage);
+
+        }
+        if (collision.GetComponent<EnemyDamage>())
+        {
+            collision.GetComponent<EnemyDamage>().Damage(spikesDamage, 0, spikeLocation);
+        }
+    }
+}
---
+++ Unity Project/Assets/RPP_Docs/RPP_Scripts/PressureSikeTrapsTRUE.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressureSikeTrapsTRUE : MonoBehaviour
+{
+    public BoxCollider pressurePlate;
+    public BoxCollider spikes;
+    public int spikesDamage;
+    private Transform spikeLocation;
+    Animator spikePressure;
+
+    //Player
+    public GameObject player;
+    public Player playerScript;
+
+    private void Start()
+    {
+        spikes.enabled = false;
+        spikePressure = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<Player>();
+        spikeLocation = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        OnTriggerEnter(pressurePlate);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player") || collision.GetComponent<EnemyDamage>())
+        {
+            Debug.Log("Someone's Ass is about to be EXPANDED");
+            if (spikes.enabled == false)
+            {
+                Debug.Log("Pressure mechanism activarted");
+                StartCoroutine(CountdownBeforeSpikes());
+            }
+            else
+            {
+                playerScript.PlayerDamage(spikesDamage);
+                collision.GetComponent<EnemyDamage>().Damage(spikesDamage, 0, spikeLocation);
+            }
+        }
+    }
+
+    IEnumerator CountdownBeforeSpikes()
+    {
+        // Prep
+        spikePressure.SetInteger("PressureSpikeInt", 1);
+        yield return new WaitForSeconds(1.5f);
+        spikes.enabled = true;
+        OnTriggerEnter(spikes);
+        // Attack
+        spikePressure.SetInteger("PressureSpikeInt", 2);
+        yield return new WaitForSeconds(1f);
+        pressurePlate.enabled = false;
+        spikes.enabled = false;
+        // Retract
+        spikePressure.SetInteger("PressureSpikeInt", 3);
+        yield return new WaitForSeconds(1f);
+        // Idle
+        spikePressure.SetInteger("PressureSpikeInt", 4);
+        pressurePlate.enabled = true;
+    }
+}
