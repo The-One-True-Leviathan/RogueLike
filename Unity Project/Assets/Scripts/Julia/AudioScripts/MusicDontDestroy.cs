a5eea526c950919a4a7d879cffd061062be4516a
LAPTOP-CQNK8T9C\ingri <j.bourdin@rubika-edu.com>
2020-11-17 20:35:21 +0100

    Intégration de musique

--- Unity Project/Assets/Scripts/Julia/AudioScripts/MusicDontDestroy.cs
+++ Unity Project/Assets/Scripts/Julia/AudioScripts/MusicDontDestroy.cs
@@ -2,17 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartPlayer : MonoBehaviour
+public class MusicDontDestroy : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        DontDestroyOnLoad(this.gameObject);
     }
 }
