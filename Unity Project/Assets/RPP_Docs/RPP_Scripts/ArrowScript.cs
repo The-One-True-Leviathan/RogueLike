312a41365e56f629c2210d030be856a5ded904ee
raphapelpal <60692478+raphapelpal@users.noreply.github.com>
2020-12-13 18:52:14 +0000

    Création pièges à flèches et derniers LDs

---
+++ Unity Project/Assets/RPP_Docs/RPP_Scripts/ArrowScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    public bool canShoot = false;
+    [SerializeField] BoxCollider arrowCollider;
+    [SerializeField] GameObject arrowObject;
+    public float arrowSpeed = 15;
+    public float arrowDamage = 4;
+
+    //Player
+    public Player playerScript;
+
+    private void Start()
+    {
+        arrowCollider = GetComponent<BoxCollider>();
+        arrowObject = this.gameObject;
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        if (canShoot)
+        {
+            StartCoroutine(ShootArrow());
+        }
+    }
+
+    IEnumerator ShootArrow()
+    {   
+        arrowObject.transform.position += new Vector3(1f, 0f, 0f) * Time.deltaTime * arrowSpeed;
+        yield return new WaitForSeconds(4f);
+        arrowObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerScript.PlayerDamage(arrowDamage);
+        }
+        if (collision.GetComponent<EnemyDamage>())
+        {
+            collision.GetComponent<EnemyDamage>().Damage(arrowDamage, 0, arrowObject.transform);
+        }
+    }
+}
---
+++ Unity Project/Assets/RPP_Docs/RPP_Scripts/RopeScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeScript : MonoBehaviour
+{
+    [SerializeField] BoxCollider boxCollider;
+    public ArrowScript arrowScript;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") || other.GetComponent<EnemyDamage>())
+        {
+            arrowScript.canShoot = true;
+        }
+    }
+}
