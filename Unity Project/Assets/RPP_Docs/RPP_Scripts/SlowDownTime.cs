d30edfb4e894131bf6b4bd51579aa2c39ba80a79
raphapelpal <60692478+raphapelpal@users.noreply.github.com>
2021-01-16 17:09:25 +0000

    Add les sons au joueur

--- Unity Project/Assets/RPP_Docs/RPP_Scripts/SlowDownTime.cs
+++ Unity Project/Assets/RPP_Docs/RPP_Scripts/SlowDownTime.cs
@@ -6,26 +6,66 @@ using UnityEngine.InputSystem;
 
 public class SlowDownTime : MonoBehaviour
 {
-    bool TimeBeingSlowed;
+    public bool timeIsBeingSlowed, canSlowDownTime;
     Controler controler;
     public AudioMixer audioMixer;
     public AudioMixerSnapshot normal, slow;
-    
+    public float currentSlowDownTimeCooldown, maxSlowDownTimeCooldown;
+
+
 
     private void Awake()
     {
         controler = new Controler();
         controler.Keyboard.Enable();
-        controler.Keyboard.SlowDownTime.performed += ctx => SlowDownMan();
+        controler.TimeControl.Enable();
+        controler.TimeControl.SlowDownTime.started += ctx => timeIsBeingSlowed = true;
+        controler.TimeControl.SlowDownTime.canceled += ctx => timeIsBeingSlowed = false;
         controler.Keyboard.NormalizeTime.performed += ctx => LeaveTimeAlone();
+        currentSlowDownTimeCooldown = maxSlowDownTimeCooldown;
+        timeIsBeingSlowed = false;
+    }
+
+    private void Update()
+    {
+        if (currentSlowDownTimeCooldown >= 0)
+        {
+            canSlowDownTime = true;
+
+        }
+        else
+        {
+            canSlowDownTime = false;
+            timeIsBeingSlowed = false;
+            controler.TimeControl.SlowDownTime.canceled += ctx => timeIsBeingSlowed = false;
+        }
+
+        if (timeIsBeingSlowed)
+        {
+            SlowDownMan();
+            currentSlowDownTimeCooldown -= Time.deltaTime;
+        }
+        else
+        {
+            if (currentSlowDownTimeCooldown < maxSlowDownTimeCooldown)
+            {
+                currentSlowDownTimeCooldown += Time.deltaTime;
+            }
+            if (currentSlowDownTimeCooldown > maxSlowDownTimeCooldown)
+            {
+                currentSlowDownTimeCooldown = maxSlowDownTimeCooldown;
+            }
+        }
     }
 
     void SlowDownMan()
-    {
-        FindObjectOfType<AudioManager>().Play("Slow Down Time");
-        Debug.Log("Time is Being Slowed");
-        Time.timeScale = 0.5f;
-        slow.TransitionTo(0.5f);
+    {        
+        if (canSlowDownTime && timeIsBeingSlowed)
+        {
+            Debug.Log("Time is Being Slowed");
+            Time.timeScale = 0.5f;
+            slow.TransitionTo(0.5f);
+        }        
     }
 
     void LeaveTimeAlone()
@@ -33,5 +73,6 @@ public class SlowDownTime : MonoBehaviour
         Debug.Log("Time Is Normal Again");
         Time.timeScale = 1f;
         normal.TransitionTo(0.5f);
+        timeIsBeingSlowed = false;
     }
 }
